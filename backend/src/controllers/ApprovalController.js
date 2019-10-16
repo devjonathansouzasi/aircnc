193839115ab8cb9074a92d8ed0ec29d575831561
Jonathan Souza <devjonathansouzasi@gmail.com>
2019-10-16 19:01:33 -0300

    Backend approvals and rejections with socket.io

--- backend/src/controllers/ApprovalController.js
+++ backend/src/controllers/ApprovalController.js
@@ -6,7 +6,15 @@ module.exports = {
     try {
       const booking = await Booking.findById(booking_id).populate("spot");
       booking.approved = true;
+
       await booking.save();
+
+      const bookingUserSocket = req.connectedUsers[booking.user];
+
+      if (bookingUserSocket) {
+        req.io.to(bookingUserSocket).emit("booking_response", booking);
+      }
+
       return res.json(booking);
     } catch (err) {
       return res.status(400).json({ error: "Error approving booking" });
--- backend/src/controllers/RejectController.js
+++ backend/src/controllers/RejectController.js
@@ -6,7 +6,14 @@ module.exports = {
     try {
       const booking = await Booking.findById(booking_id).populate("spot");
       booking.approved = false;
+
       await booking.save();
+
+      const bookingUserSocket = req.connectedUsers[booking.user];
+
+      if (bookingUserSocket) {
+        req.io.to(bookingUserSocket).emit("booking_response", booking);
+      }
       return res.json(booking);
     } catch (err) {
       return res.status(400).json({ error: "Error rejecting booking" });
